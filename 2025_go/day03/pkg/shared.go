3be4056a3fb6e160ba865508d9d844992a01e78e
coltn <mail@coltn.io>
2025-12-12 10:32:17 -0700

    2025_go: complete day03/part02; remove input and answer data as requested by the faq for AoC

--- 2025_go/day03/pkg/shared.go
+++ 2025_go/day03/pkg/shared.go
@@ -7,10 +7,12 @@ import (
 )
 
 func getBankJoltageBeforeY(line string, y int) (int, string, error) {
+	fmt.Printf("debug: contents of line %v\n line len: %d\n", line, len(line))
 
 	l := len(line)
 	max := 0
 	searching := line[:l - y]
+	fmt.Printf("debug: searching %v\n searching len: %d\n", searching, len(searching))
 	var leftover string
 
 	for i, r := range searching {
@@ -75,25 +77,36 @@ func getBankJoltage(line string) (int, error) {
 
 func FindAmountOfJoltages(scanner *bufio.Scanner, amount int) ([]int, error) {
 	var joltages []int
+	debug_i := 0
 	for scanner.Scan() {
+
+		fmt.Printf("debug: line number %v\n", debug_i)
+		debug_i ++
+
 		bank := scanner.Text()
 		bank_total := 0
 		var joltage int
-		for ;amount >= 0; amount-- {
+		for i := amount - 1; i >= 0; i-- {
+			fmt.Printf("debug: amount left: %v\n", i)
 			var err error
-			joltage, bank, err = getBankJoltageBeforeY(bank, amount)
+			joltage, bank, err = getBankJoltageBeforeY(bank, i)
+
+			fmt.Printf("debug: joltage amount %v\n", joltage)
+
 			if err != nil {
 				err = fmt.Errorf("Could not getBankJoltage on bank: %v\n got error: %v\n", bank, err)
 			}
 
-			if amount > 0 {
-				bank_total += joltage * int(math.Pow10(amount))
+			if i > 0 {
+				bank_total += joltage * int(math.Pow10(i))
 			} else {
 				bank_total += joltage
 			}
 		}
 
-		joltages = append(joltages, joltage)
+		fmt.Printf("debug: bank_total: %v\n", bank_total)
+
+		joltages = append(joltages, bank_total)
 	}
 
 	return joltages, nil
